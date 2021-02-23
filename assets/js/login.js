c3920a28cbbc9b1d4db2f61cd8e62b87e5ed6839
xjm <2889073149@qq.com>
2021-02-23 13:53:53 +0800

    test

---
+++ assets/js/login.js
@@ -0,0 +1,76 @@
+$(function () {
+    $("#linkLogin").on("click", function () {
+        $(".reg-box").show()
+        $(".login-box").hide()
+    })
+    $("#linkReg").on("click", function () {
+        $(".reg-box").hide()
+        $(".login-box").show()
+    })
+
+    const form = layui.form;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, "密码必须6~12位，且不能出现空格"],
+        repwd: function (val) {
+            const vals = $(".reg-box [name=password]").val()
+            if (vals !== val) {
+                return "两次密码不一致"
+            }
+        }
+    });
+
+    //注册
+    $("#form-all").on("submit", function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "http://ajax.frontend.itheima.net/api/reguser",
+            data: {
+                username: $(".reg-box [name=username]").val(),
+                password: $(".reg-box [name=password]").val()
+            },
+            success: res => {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+
+                }
+                layer.msg("注册成功，请登录")
+                $("#linkReg").click();
+            }
+
+        });
+
+        // $("#form-lg [name=username]").val()=
+    });
+    //登录
+    $("#form-lg").on("submit", function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "http://ajax.frontend.itheima.net/api/login",
+            data: $(this).serialize(),
+            success: res => {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+
+                }
+                localStorage.setItem("broken", res.token);
+                location.href = "index.html"
+            }
+
+        });
+
+        // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+        // 会先调用 ajaxPrefilter 这个函数
+        // 在这个函数中，可以拿到我们给Ajax提供的配置对象
+        $.ajaxPrefilter(function (options) {
+            // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+            options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+        })
+    })
+
+})
+
